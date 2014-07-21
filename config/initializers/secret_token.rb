8346b5e2880c66559c2ab7d0d4df54bb94b2b715
Justin Chan <justin.the.c@gmail.com>
2014-07-21 06:10:31 -0400

    Generated Question Scaffold, Integrated Bootstrap 3, and redesigned the Question Index page.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quizgenerator::Application.config.secret_token = 'ef6d7306ec7e29e43c168a6e8e334f13f61b82f8ff63216c98183ef482d80c5b0b75c3f9a4d2f8683490122dbddb0b630b63f2c3ba0773cb78fe98a0b386963e'
